3dc3273b8771101e62095ee5169bb4c517dfa3d7
Kristina Antonava <kristy_92@inbox.ru>
2022-10-24 12:06:01 +0300

    some corrections

--- HW_5/task_2.ts
+++ HW_5/task_2.ts
@@ -1,6 +1,6 @@
 function credit(creditSum: number) {
-  let procent = 17;
-  let year = 5;
+  const procent = 17;
+  const year = 5;
   let overPay = ((creditSum * procent) / 100) * year;
   return overPay;
 }
--- HW_5/task_4.ts
+++ HW_5/task_4.ts
@@ -1,5 +1,5 @@
 function getSumNumbers(userNumber: string) {
-  let figures = "" + userNumber;
+  const figures = "" + userNumber;
   let sum = 0;
   for (let i = 0; i < figures.length; i++) {
     sum += +figures[i];
