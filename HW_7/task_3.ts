68d51c6ee9e6659d0154c7b460fec331043dee48
Kristina Antonava <kristy_92@inbox.ru>
2022-11-01 13:04:15 +0300

    HW_7

---
+++ HW_7/task_3.ts
@@ -0,0 +1,2 @@
+const numbers = [5, 43, 63, 23, 90];
+console.log(numbers.splice(numbers.length));
---
+++ HW_7/task_4.ts
@@ -0,0 +1,4 @@
+const students = ["Polina", "Dasha", "Masha"];
+students.splice(-1, 1, "Borya");
+students.splice(0, 1, "Andrey");
+console.log(students);
